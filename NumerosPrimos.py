d0cfb47fa9f1923022e82ae1028846dad286f7d7
Aluno da Infinity School <100245367+RayanLopes@users.noreply.github.com>
2022-08-15 19:44:22 -0300

    atlz

--- NumerosPrimos.py
+++ NumerosPrimos.py
@@ -1,11 +1,12 @@
 numero = int(input('Digite um número:'))
 total = 0
+verde = '\033[1;36;42m'
 for var in range (1, numero + 1):
     if numero % var == 0:
-    #     print('\033', end='')
-        total += 1
-    # else:
-    #     print('\033', end='')   
+         print(f'{verde}', end='')
+         total += 1
+    else:
+        print(f'{verde}', end='')   
         print(f'{var}', end='')
 print(f'O número {numero} foi divisível {total} vezes.')
 if total == 2:
